3710fce8ad4f49484b2837bff8782d5b70b66dc5
unknown <theo.lecointe@auer.fr>
2022-12-08 11:14:57 +0100

    v1.0.5

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 /* import '@aws-amplify/ui-react/styles.css'; */
 
 import awsExports from './aws-exports';
+import logo from './intuis.gif'; 
+import './App.css';
+
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
+function App(/*{ signOut, user }*/) {
     return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenue sur la plateforme de supervision d'installations ZéPAC
+        </p>
+        <a
+          className="App-link"
+          href="https://www.auer.fr/fr/produits/chauffe-eau-thermodynamique-et-eau-chaude-sanitaire-collectif-zepac70/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          En savoir plus sur le ZéPAC
+        </a>
+      </header>
+    </div>
   );
 }
 
